351d628ca24278d16adc47b2174c6a6010c4456e
Bjørn Gustav Baklid <bg@dyret.net>
2016-05-21 21:43:29 +0200

    Basic script
    Able to download past tweets to CSV

---
+++ St1TwitterMiner.py
@@ -0,0 +1,67 @@
+﻿# encoding: utf-8
+
+#import api key variables from file
+from apikeys import *
+
+#import modules
+import tweepy
+import csv
+
+def parse_tweet(text):
+    split = text.split('\n')
+    gasoline = split[1].split(': ')[1]
+    diesel = split[2].split(': ')[1]
+    station = split[3]
+    time = split[5]
+    return time,station,gasoline,diesel
+
+def get_all_tweets(screen_name):
+    # Twitter only allows access to a users most recent 3240 tweets with this method
+
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_secret)
+    api = tweepy.API(auth)
+
+    # Initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    #make initial requst for the most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name, count=200)
+
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        print ("Getting Tweets before %s" % (oldest))
+
+        #all subsequent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name, count=200, max_id=oldest)
+
+        #save the most recent tweets
+        alltweets.extend(new_tweets)
+
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+
+        print ("...%s tweets downloaded so far" % (len(alltweets)))
+
+    # transform the tweepy tweets into a 2D array that will populate the csv
+    outtweets = []
+    for tweet in alltweets:
+        outtweets.append(parse_tweet(tweet.text))
+
+    #write the csv
+    with open('%s_tweets.csv' % screen_name, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["created_at","tid","stasjon","bensin","diesel"])
+        writer.writerows(outtweets)
+    pass
+
+if __name__ == '__main__':
+    #pass in the username of the account you want to download
+    get_all_tweets("St1Norge")
